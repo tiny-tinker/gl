042d5375f4a916f60c1f8aab6e6da4c612cd9248
Mark <mwbdevelopment@gmail.com>
2017-12-20 11:57:00 -0500

    bootstrap dashboard theme integration

--- src/app/app.js
+++ src/app/app.js
@@ -20,7 +20,7 @@ import { useScroll } from 'react-router-scroll';
 import '!style-loader!css-loader!sanitize.css/sanitize.css';
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css';
 import '!style-loader!css-loader!global.css';
-import '!style-loader!css-loader!../assets/theme-dashboard/dist/toolkit-inverse.min.css';
+import '!style-loader!css-loader!sass-loader!../assets/theme-dashboard/scss/toolkit-inverse.scss';
 
 // Import selector for `syncHistoryWithStore`
 import { makeSelectLocationState } from 'containers/App/selectors';
